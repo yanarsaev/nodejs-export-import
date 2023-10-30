5c5c17538771b709ce33fb6518746513bd82cdc4
yanarsaev <yanarsaev-it@internet.ru>
2023-10-30 13:26:30 +0300

    first commit

---
+++ data/second.js
@@ -0,0 +1,3 @@
+import obj from '../index.js'
+
+console.log(obj.func(7, 8), obj.num, obj.str)
---
+++ index.js
@@ -0,0 +1,8 @@
+export default {
+    "func": function(num_1, num_2) {
+        console.log(num_1 * num_2)
+    },
+
+    "num": 96,
+    "str": "intocode"
+}
